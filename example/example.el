ef4064cff0b13567dcda898573120182aa31303d
rubikitch <rubikitch@ruby-lang.org>
2016-08-01 07:45:55 +0900

    example.el: F6 -> SPC

--- example/example.el
+++ example/example.el
@@ -4,7 +4,7 @@
 
 ;; This is a simple demonstration that shows off a few of the
 ;; functions and options of the demo-it system.  Evaluating this
-;; buffer starts off the presentation, and hitting F6 steps through
+;; buffer starts off the presentation, and hitting SPC steps through
 ;; the demonstration.
 
 ;;; Code:
